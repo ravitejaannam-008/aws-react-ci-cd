566c8d28bbe30fc75d618d23b550d4cca7d3da85
RAVITEJA ANNAM <150829995+ravitejaannam-008@users.noreply.github.com>
2025-04-25 03:41:18 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Welcome to AWS CI/CD React App</h1>
+      <h2>by RAVI TEJA ANNAM</h2>
+    </div>
+  );
+}
+
+export default App;
